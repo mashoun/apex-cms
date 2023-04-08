8fc4279640838914172519194e22317065872325
mashoun <mashoun.me@gmail.com>
2023-04-08 16:03:37 +0300

    profile view is styled

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,10 @@
 <template>
-  <header class="w-100 p-3 d-flex justify-content-between align-items-center shadow-sm pop">
+  <header class="w-100 position-fixed bg-light top-0 start-0 z-3 p-3 d-flex justify-content-between align-items-center shadow-sm pop">
     <section class="w-100 d-flex align-items-center justify-content-between">
       <div class="d-flex gap-3 align-items-center">
-        <div class="position-relative">    
+        <div class="position-relative point" title="check notifications">    
           <img :src="pic" data-bs-toggle="modal" data-bs-target="#notifications" alt="logo" class="img-fluid rounded-circle" width="35" height="35">
           <span class="position-absolute top-0 start-100 translate-middle badge border border-light rounded-circle bg-danger p-1"><span class="visually-hidden">unread messages</span></span>
-          <!-- Modal -->
-          <div class="modal fade" id="notifications" tabindex="-1" aria-labelledby="notifications" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h1 class="modal-title fs-6" id="exampleModalLabel">You have 4 notifications</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                  <div class="p-2">
-                    <span class="fs-xsmall pop">22 hrs ago</span>
-                    <div class="d-flex align-items-center gap-2">
-                      <i class="bi bi-camera-reels text-info"></i>
-                      <small class="pop text-secondary">mokhtar.ayoubi@live.com want to meet !</small>
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="p-2">
-                    <span class="fs-xsmall pop">22 hrs ago</span>
-                    <div class="d-flex align-items-center gap-2">
-                      <i class="bi bi-star text-danger"></i>
-                      <small class="pop text-secondary">mohamad.ayoubi@gmail.com have subscribed to your newsLetter !</small>
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="p-2">
-                    <span class="fs-xsmall pop">22 hrs ago</span>
-                    <div class="d-flex align-items-center gap-2">
-                      <i class="bi bi-chat-square-dots text-warning"></i>
-                      <small class="pop text-secondary">yarahoro7@gmail.com commented on a blog !</small>
-                    </div>
-                  </div>
-
-
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="modal">Clear</button>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
         <h5 class="text-secondary fs-4 m-0">Mashoun CMS</h5>
       </div>
@@ -64,9 +23,51 @@
       <router-link class="link-underline-opacity-100-hover link-primary link-offset-3 link-underline-opacity-0"  to="/analytics">Analytics</router-link>
     </nav>
   </header>
-  <main class="py-3">
+  <main class="py-5 my-5">
     <router-view></router-view>
   </main>
+  
+  <!-- Modals -->
+  <div class="modal fade" id="notifications" tabindex="-1" aria-labelledby="notifications" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-6" id="exampleModalLabel">You have 4 notifications</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="p-2">
+            <span class="fs-xsmall pop">22 hrs ago</span>
+            <div class="d-flex align-items-center gap-2">
+              <i class="bi bi-camera-reels text-info"></i>
+              <small class="pop text-secondary">mokhtar.ayoubi@live.com want to meet !</small>
+            </div>
+          </div>
+          <hr>
+          <div class="p-2">
+            <span class="fs-xsmall pop">22 hrs ago</span>
+            <div class="d-flex align-items-center gap-2">
+              <i class="bi bi-star text-danger"></i>
+              <small class="pop text-secondary">mohamad.ayoubi@gmail.com have subscribed to your newsLetter !</small>
+            </div>
+          </div>
+          <hr>
+          <div class="p-2">
+            <span class="fs-xsmall pop">22 hrs ago</span>
+            <div class="d-flex align-items-center gap-2">
+              <i class="bi bi-chat-square-dots text-warning"></i>
+              <small class="pop text-secondary">yarahoro7@gmail.com commented on a blog !</small>
+            </div>
+          </div>
+
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="modal">Clear</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
--- src/views/profile.vue
+++ src/views/profile.vue
@@ -1,19 +1,123 @@
 <template>
-    <section class="container">
+    <section class="container gap-5 d-flex flex-column">
         <!-- Contact info -->
-        
-        <h5 class="font-monospace">Edit Contact Info</h5>
         <div class="row g-3">
+            <div class="col-12 d-flex justify-content-between">
+                <h5 class="pop text-center text-primary text-lg-start">Contact Info</h5>
+                <button class="btn btn-sm btn-outline-primary">Save profile</button>
+            </div>
+            <div class="col-12">
+                <img src="https://picsum.photos/200" alt="profile pic" width="70" height="70" class="img-fluid rounded-circle shadow-sm">
+                <div class="mt-3">
+                    <label for="formFile" class="form-label ps-1 fs-smaller pop">Set new profile pic </label>
+                    <input type="file" class="form-control" id="inputGroupFile04" accept="image/png, image/jpg, image/jpeg">
+                </div>
+            </div>
+            
             <div class="col-12 col-lg-6">
-                <div class="">
+                <div class="form-floating">
+                    <textarea class="form-control" rows="4" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                    <label for="floatingTextarea">Your bio</label>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInput1" placeholder="name@example.com">
+                    <label for="floatingInput1">Email address</label>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInput2" placeholder="name@example.com">
+                    <label for="floatingInput2">Phone number</label>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInput3" placeholder="name@example.com">
+                    <label for="floatingInput3">Whatsapp call</label>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInput4" placeholder="name@example.com">
+                    <label for="floatingInput4">Whatsapp chat</label>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInput5" placeholder="name@example.com">
+                    <label for="floatingInput5">Whatsapp group</label>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInput6" placeholder="name@example.com">
+                    <label for="floatingInput6">LinkedIn</label>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInput7" placeholder="name@example.com">
+                    <label for="floatingInput7">Github</label>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInput8" placeholder="name@example.com">
+                    <label for="floatingInput8">Youtube</label>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInput9" placeholder="name@example.com">
+                    <label for="floatingInput9">Stack overflow</label>
+                </div>
+            </div>
+        </div>
+
+        <!-- Adding Links -->
+        <div class="row g-3">
+            <div class="col-12 d-flex justify-content-between">
+                <h5 class="pop text-primary text-center text-lg-start">Custom links</h5>
+                <button class="btn btn-sm btn-outline-primary">Add link</button>
+                <!-- <span class="material-symbols-outlined fs-3 text-primary point" title="add link">add_link</span> -->
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Link title</label>
                 </div>
             </div>
             <div class="col-12 col-lg-6">
                 <div class="form-floating">
                     <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Email address</label>
+                    <label for="floatingInput">Link URL</label>
                 </div>
             </div>
         </div>
+
+        <!-- Adding Slides -->
+        <div class="row g-3">
+            <div class="col-12 d-flex justify-content-between">
+                <h5 class="pop text-primary text-center text-lg-start">Image slides</h5>
+                <button class="btn btn-sm btn-outline-primary">Add Image</button>
+                <!-- <span class="material-symbols-outlined fs-3 text-primary point" title="save image">save</span> -->
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="mb-3">
+                    <label for="formFile" class="form-label ps-1 fs-smaller pop">Alt text</label>
+                    <input class="form-control" type="text" id="formFile">
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="mb-3">
+                    <label for="formFile" class="form-label ps-1 fs-smaller pop">Select image </label>
+                    <input class="form-control" type="file" id="formFile"  accept="image/png, image/jpg, image/jpeg">
+                </div>
+            </div>
+        </div>
+
+
     </section>
 </template>
